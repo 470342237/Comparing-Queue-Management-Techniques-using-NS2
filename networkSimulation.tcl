81dfaf8f8ff883f1be8bdd03adf9578ca0097c11
Sumit Keswani <sumitkeswani123@gmail.com>
2016-03-03 20:27:46 +0530

    Updated comments

--- networkSimulation.tcl
+++ networkSimulation.tcl
@@ -9,14 +9,13 @@ $ns color 2 Red
 set val(x) 500
 set val(y) 500
 
-
 # Define options
 set val(chan) Channel/WirelessChannel    ;# channel type
 set val(prop) Propagation/TwoRayGround   ;# radio-propagation model
 
 set val(netif)          Phy/WirelessPhy            ;# network interface type
 set val(mac)            Mac/802_11                 ;# MAC type
-set val(ifq)            Queue/DropTail/PriQueue    ;# interface queue type
+set val(ifq)            Queue/DropTail/PriQueue    ;# interface queue type. This can be changed.
 set val(ll)             LL                         ;# link layer type
 set val(ant)            Antenna/OmniAntenna        ;# antenna model
 set val(ifqlen)         50                         ;# max packet in ifq
